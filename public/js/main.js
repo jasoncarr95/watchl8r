10c3d0b7fb6daf3a7a9fbcd288631c409d053f51
jasoncarr95 <jason@jasoncarr.dev>
2022-07-10 23:26:37 -0400

    replaced trash can/delete function with a checkmark/cross off function

--- public/js/main.js
+++ public/js/main.js
@@ -1,34 +1,39 @@
-const deleteText = document.querySelectorAll('.fa-trash')
-const thumbText = document.querySelectorAll('.fa-thumbs-up')
+const deleteBtn = document.querySelectorAll('.fa-box-check')
+const item = document.querySelectorAll('.item span')
+const itemCompleted = document.querySelectorAll('.item span.completed')
 
-Array.from(deleteText).forEach((element)=>{
-    element.addEventListener('click', deleteItem)
+Array.from(deleteBtn).forEach((element)=>{
+    element.addEventListener('click', markComplete)
 })
 
-Array.from(thumbText).forEach((element)=>{
-    element.addEventListener('click', addLike)
+Array.from(item).forEach((element)=>{
+    element.addEventListener('click', markComplete)
 })
 
-async function deleteItem(){
-    const sName = this.parentNode.childNodes[1].innerText
-    const bName = this.parentNode.childNodes[3].innerText
-    try{
-        const response = await fetch('deleteItem', {
-            method: 'delete',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-              'titleS': sName,
-              'birthNameS': bName
-            })
-          })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
+Array.from(itemCompleted).forEach((element)=>{
+    element.addEventListener('click', markUnComplete)
+})
 
-    }catch(err){
-        console.log(err)
-    }
-}
+// async function deleteItem(){
+//     const sName = this.parentNode.childNodes[1].innerText
+//     const bName = this.parentNode.childNodes[3].innerText
+//     try{
+//         const response = await fetch('deleteItem', {
+//             method: 'delete',
+//             headers: {'Content-Type': 'application/json'},
+//             body: JSON.stringify({
+//               'titleS': sName,
+//               'birthNameS': bName
+//             })
+//           })
+//         const data = await response.json()
+//         console.log(data)
+//         location.reload()
+
+//     }catch(err){
+//         console.log(err)
+//     }
+// }
 
 async function addLike(){
     const sName = this.parentNode.childNodes[1].innerText
@@ -42,6 +47,44 @@ async function addLike(){
               'titleS': sName,
               'birthNameS': bName,
               'likesS': tLikes
+            })
+          })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+
+    }catch(err){
+        console.log(err)
+    }
+}
+
+async function markComplete(){
+    const itemText = this.parentNode.childNodes[1].innerText
+    try{
+        const response = await fetch('markComplete', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                'itemFromJS': itemText
+            })
+          })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+
+    }catch(err){
+        console.log(err)
+    }
+}
+
+async function markUnComplete(){
+    const itemText = this.parentNode.childNodes[1].innerText
+    try{
+        const response = await fetch('markUnComplete', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                'itemFromJS': itemText
             })
           })
         const data = await response.json()
