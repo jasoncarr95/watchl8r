1f1f01f6250550d3e9a2fab7bfc0c2449d60c425
jasoncarr95 <jason@jasoncarr.dev>
2022-10-01 02:13:58 -0400

    Clean up platform logo rendering logic
    Delete code no longer used.

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose')
 const passport = require('passport')
 const session = require('express-session')
 const MongoStore = require('connect-mongo')(session)
+const methodOverride = require("method-override");
 const flash = require('express-flash')
 const logger = require('morgan')
 const connectDB = require('./config/database')
@@ -19,32 +20,43 @@ require("dotenv").config({ path: "./config/.env" });
 // Passport config
 require('./config/passport')(passport)
 
+//Connect To Database
 connectDB()
 
-// EJS
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-app.use(logger('dev'))
-// Sessions
+//Using EJS for views
+app.set("view engine", "ejs");
+
+//Static Folder
+app.use(express.static("public"));
+
+//Body Parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Logging
+app.use(logger("dev"));
+
+//Use forms for put / delete
+app.use(methodOverride("_method"));
+
+// Setup Sessions - stored in MongoDB
 app.use(
-    session({
-      secret: 'keyboard cat',
-      resave: false,
-      saveUninitialized: false,
-      store: new MongoStore({ mongooseConnection: mongoose.connection }),
-    })
-  )
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  })
+);
   
 // Passport middleware
 app.use(passport.initialize())
 app.use(passport.session())
 
-app.use(flash())
+//Use flash messages for errors, info, ect...
+app.use(flash());
   
-
-// ROUTES
+//Setup Routes For Which The Server Is Listening
 app.use('/', homeRoutes)
 app.use('/watchList', watchListRoutes)
  
@@ -55,41 +67,4 @@ app.listen(process.env.PORT || PORT, () => {
 });
 
 
-// Server Responds on main page with rendered EJS... INFO
 
-app.get("/", async (request, response) => {
-  const movies = await db
-    .collection("movies")
-    .find({ type: "movie" })
-    .toArray();
-  const shows = await db.collection("movies").find({ type: "show" }).toArray();
-
-  console.log(`Received a GET a request on '/'`);
-  response.render("index.ejs", { movieItem: movies, showItem: shows });
-});
-
-
-
-
-
-app.get("/movies", async (request, response) => {
-  const movies = await db
-    .collection("movies")
-    .find({ type: "movie" })
-    .toArray();
-  const shows = await db.collection("movies").find({ type: "show" }).toArray();
-
-  console.log(`Received a GET a request on '/movies'`);
-  response.render("movies.ejs", { movieItem: movies, showItem: shows });
-});
-
-app.get("/shows", async (request, response) => {
-  const movies = await db
-    .collection("movies")
-    .find({ type: "movie" })
-    .toArray();
-  const shows = await db.collection("movies").find({ type: "show" }).toArray();
-
-  console.log(`Received a GET a request on '/shows'`);
-  response.render("shows.ejs", { movieItem: movies, showItem: shows });
-});
